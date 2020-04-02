68ee3a744e5475353b3cb27897d445867d940907
Chris Pondoc <chris.pondoc@comcast.net>
2020-04-01 21:58:20 -0400

    Add files via upload

---
+++ Chapter 1/SumOddNumbers.cpp
@@ -0,0 +1,19 @@
+/* Sum Odd Numbers */
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	// Important local variables
+	int n;
+	
+	// Header for output and input
+	cout << "This program calculates the sum of the first n odd numbers." << endl;
+	cout << "Input a value of n: ";
+	cin >> n;
+	
+	// Calculation and return of output
+	cout << "The sum of the first n odd integers is: " << n * n << endl;
+	
+	return 0;
+}
